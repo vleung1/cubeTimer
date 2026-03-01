e313b2a8a6775ea38ff96e2cb81e96ec3798c00a
VincentLeung <vleung1@gmail.com>
2026-03-01 08:22:41 -0500

    Initial Commit

---
+++ cubeTimer/cubeTimerApp.swift
@@ -0,0 +1,32 @@
+//
+//  cubeTimerApp.swift
+//  cubeTimer
+//
+//  Created by Vincent Leung on 3/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct cubeTimerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
